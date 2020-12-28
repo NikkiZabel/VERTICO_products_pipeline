16ebfc9e5cb3f88aa09ad4956632aa94ab439660
NikkiZabel <zabelnj@cardiff.ac.uk>
2020-12-28 12:56:21 +0100

    Extract inclination and position angle from Toby's table

--- heracles.py
+++ heracles.py
@@ -3,6 +3,7 @@ from create_moments import *
 import warnings; warnings.filterwarnings("ignore")
 import os
 from glob import glob
+from astropy.io import fits
 
 sample = 'heracles'
 version = '1_0'
@@ -13,6 +14,17 @@ overwrite = True
 sun = True
 tosave = True
 resolution = 1200
+
+def get_inc_pa(galaxy, ):
+    table = fits.open(path + 'heracles_sdss_r_properties.fits')[1]
+    gal_name_table = table.data['Galaxy']
+    gal_num_table = np.array([n.split('C')[1] for n in gal_name_table])
+
+    galaxy_num = galaxy.split('c')[1]
+    incl = table.data['inclination'][gal_num_table == galaxy_num]
+    pa = table.data['pa'][gal_num_table == galaxy_num]
+
+    return incl[0], pa[0]
 
 if resolution == 1200:
     data = [f for f in glob(path + '*1200pc.fits')]
@@ -32,6 +44,8 @@ for file in data:
 
         file_pbcorr = file
         file_uncorr = file
+
+        inc, pa = get_inc_pa(galaxy)
 
         if not os.path.exists(path + 'products_v' + version + '/' + str(resolution) + 'pc/sun18_method/' + galaxy + '/'):
             os.mkdir(path + 'products_v' + version + '/' + str(resolution) + 'pc/sun18_method/' + galaxy + '/')
@@ -53,15 +67,15 @@ for file in data:
         #    continue
         savepath = path + 'products_v' + version + '/smoothed_cubes_' + resolution + 'pc/sun18_method/' + galaxy + '/'
 
-    CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
-                  sun=sun, tosave=tosave, sample=sample).moment_zero(units='K km/s')
-    CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
-                  sun=sun, tosave=tosave, sample=sample).moment_zero()
-    CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
-                  sun=sun, tosave=tosave, sample=sample).moment_zero(peak=True)
-    CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
-                 sun=sun, tosave=tosave, sample=sample).moment_1_2()
-    CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
-                 sun=sun, tosave=tosave, sample=sample).moment_1_2(moment=2)
+    #CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
+    #              sun=sun, tosave=tosave, sample=sample).moment_zero(units='K km/s')
+    #CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
+    #              sun=sun, tosave=tosave, sample=sample).moment_zero()
+    #CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
+    #              sun=sun, tosave=tosave, sample=sample).moment_zero(peak=True)
+    #CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
+    #             sun=sun, tosave=tosave, sample=sample).moment_1_2()
+    #CreateImages(galaxy, file_pbcorr, file_uncorr, savepath=savepath, refresh=refresh, overwrite=overwrite,
+    #             sun=sun, tosave=tosave, sample=sample).moment_1_2(moment=2)
 
-    #break
+    break
